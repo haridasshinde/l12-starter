38c10101733a58e81c706e9634c7e7fba3090a32
Haridas Shinde <haridas@tmbill.com>
2025-08-09 10:23:23 +0530

    chore: drop specific tables before migreate

---
+++ database/migrations/0000_01_01_000000_drop_specific_tables_before_migrate.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        $tables = [
+            // 'users',
+            'password_reset_tokens',
+            'sessions',
+            'cache',
+            'cache_locks',
+            'jobs',
+            'job_batches',
+            'failed_jobs',
+        ];
+
+        foreach ($tables as $table) {
+            if (Schema::hasTable($table)) {
+                Schema::drop($table);
+            }
+        }
+    }
+
+    public function down(): void
+    {
+        // No rollback needed for table drops
+    }
+};
